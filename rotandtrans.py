71fa7ad12d300cfb2db59f405842d1652b27425c
Peter Tarca <ptarca@astrocsys.com>
2017-08-14 00:05:57 -0400

    placed rotandtran in repository

---
+++ rotandtrans.py
@@ -0,0 +1,67 @@
+
+import cv2
+import numpy as np
+ 
+# Read Image
+im = cv2.imread("c:/users/peter/desktop/ht_validation/aug3captures/rightCamtake1frame233.jpg");
+size = im.shape
+     
+#2D image points. If you change the image, you need to change vector
+image_points = np.array([
+                            (193,288),     # left frame reflector
+                            (287,287),    # right frame reflector
+                            (288, 282),    # back frame reflector
+                            (258, 231),     # ball ctr
+                            (252,231),     # ball left
+                            (266, 231)     # ball right
+                        ], dtype="double")
+ 
+# 3D object points.
+object_points = np.array([
+                            (875.0, 762.0, 2621.4), # left frame reflector
+                            (1275.0, 762.0, 2621.4), # right frame reflector
+                            (1275.0, 762.0, 2921.4), # back frame reflector
+                            (1422.4, 1041.4, 2884.57),   # ball ctr
+                            (1385.57, 1041.4, 2884.57),  # ball left
+                            (1459.23, 1041.4, 2884.57)  # ball right
+                        ])
+ 
+ 
+# Camera internals
+ 
+focal_length = size[1]
+center = (size[1]/2, size[0]/2)
+camera_matrix = np.array(
+                         [[602.3300926, 0, 314.00816007],
+                         [0, 601.89164578, 265.7839791],
+                         [0, 0, 1]], dtype = "double"
+                         )
+ 
+print "Camera Matrix :\n {0}".format(camera_matrix)
+ 
+#dist_coeffs = np.zeros((4,1)) # Assuming no lens distortion
+dist_coeffs = np.array([-0.13277752, 0.24566243, 0.00257889, -0.00288082, -0.35018818]) # lens distortion
+
+(success, rotation_vector, translation_vector) = cv2.solvePnP(object_points, image_points, camera_matrix, dist_coeffs, flags=cv2.CV_ITERATIVE)
+ 
+print "Rotation Vector:\n {0}".format(rotation_vector)
+print "Translation Vector:\n {0}".format(translation_vector)
+ 
+(nose_end_point2D, jacobian) = cv2.projectPoints(np.array([(1385.57, 1041.4, 2884.57)]), rotation_vector, translation_vector, camera_matrix, dist_coeffs)
+ 
+for p in image_points:
+    cv2.circle(im, (int(p[0]), int(p[1])), 3, (0,0,255), -1)
+ 
+ 
+print int(nose_end_point2D[0][0][0]), int(nose_end_point2D[0][0][1])
+
+
+p1 = ( int(image_points[0][0]), int(image_points[0][1]))
+p2 = ( int(nose_end_point2D[0][0][0]), int(nose_end_point2D[0][0][1]))
+ 
+cv2.line(im, p2, p2, (255,0,0), 2)
+ 
+# Display image
+cv2.imshow("Output", im)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
---
+++ stereo_cal.py
@@ -0,0 +1,181 @@
+import numpy as np
+import cv2
+
+numBoards = 50  #how many boards would you like to find
+board_w = 9
+board_h = 6
+
+board_sz = (9,6)
+board_n = board_w*board_h
+
+# termination criteria
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+# Arrays to store object points and image points from all the images.
+object_points = [] # 3d point in real world space
+imagePoints1 = [] # 2d points in image plane.
+imagePoints2 = [] # 2d points in image plane.
+
+corners1 = []
+corners2 = []
+
+#obj = []
+#for j in range(0,board_n):
+    #obj.append(np.(j/board_w, j%board_w, 0.0))
+obj = np.zeros((6*9,3), np.float32)
+obj[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
+
+cameraL = cv2.VideoCapture('C:/Users/peter/Desktop/ht_validation/calibration/intrinsic cal-left cam.avi')
+if cameraL.isOpened() == False:
+        print "Cannot open input video!"
+        exit()
+cameraL.set(1, 100) #CV_CAP_PROP_POS_FRAMES
+
+cameraR = cv2.VideoCapture('C:/Users/peter/Desktop/ht_validation/calibration/intrinsic cal-right cam.avi')
+if cameraR.isOpened() == False:
+        print "Cannot open input video!"
+        exit()
+cameraR.set(1, 100) #CV_CAP_PROP_POS_FRAMES
+
+success = 0
+k = 0
+found1 = False
+found2 = False
+
+while (success < numBoards):
+
+   # grab the left frame
+   (grabbed, img1) = cameraL.read()
+   height, width, depth  = img1.shape
+   # grab the right frame
+   (grabbed, img2) = cameraR.read()
+   gray1 = cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
+   gray2 = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
+
+   found1, corners1 = cv2.findChessboardCorners(img1, board_sz)
+   found2, corners2 = cv2.findChessboardCorners(img2, board_sz)
+
+   if (found1):
+       cv2.cornerSubPix(gray1, corners1, (11, 11), (-1, -1),criteria)
+       cv2.drawChessboardCorners(gray1, board_sz, corners1, found1)
+
+   if (found2):
+       cv2.cornerSubPix(gray2, corners2, (11, 11), (-1, -1), criteria)
+       cv2.drawChessboardCorners(gray2, board_sz, corners2, found2)
+
+   cv2.imshow('image1', gray1)
+   cv2.imshow('image2', gray2)
+
+   k = cv2.waitKey(100)
+   print k
+   if (k == 27): #ESC key
+       break
+   if (found1 != 0 and found2 != 0):
+
+       imagePoints1.append(corners1);
+       imagePoints2.append(corners2);
+       object_points.append(obj);
+       print "Corners stored\n"
+       success+=1
+
+       if (success >= numBoards):
+           break
+
+cv2.destroyAllWindows()
+cameraL.release()
+cameraR.release()
+print "Starting Calibration\n"
+cameraMatrix1 = np.array(
+                         [[610.07804118, 0, 314.48633303],
+                         [0, 609.62733791, 265.13152021],
+                         [0, 0, 1]], dtype = "double"
+                         )
+ 
+distCoeffs1 = np.array([-0.14480132, 0.28330372, 0.00185214, -0.00103649, -0.33967936]) # lens distortion
+cameraMatrix2 = np.array(
+                         [[602.3300926, 0, 314.00816007],
+                         [0, 601.89164578, 265.7839791],
+                         [0, 0, 1]], dtype = "double"
+                         )
+ 
+distCoeffs2 = np.array([-0.13277752, 0.24566243, 0.00257889, -0.00288082, -0.35018818]) # lens distortion
+
+
+retval, cameraMatrix1, distCoeffs1, cameraMatrix2, distCoeffs2, R, T, E, F = cv2.stereoCalibrate(object_points, imagePoints1, imagePoints2, (width, height),
+                                                                                                 cameraMatrix1, distCoeffs1, cameraMatrix2, distCoeffs2,
+                                                                                                 flags=cv2.CALIB_FIX_INTRINSIC + cv2.CALIB_USE_INTRINSIC_GUESS,
+                                                                                                 criteria = criteria)
+## , cv2.cvTermCriteria(cv2.CV_TERMCRIT_ITER+cv2.CV_TERMCRIT_EPS, 100, 1e-5),   cv2.CV_CALIB_SAME_FOCAL_LENGTH | cv2.CV_CALIB_ZERO_TANGENT_DIST)
+#cv2.cv.StereoCalibrate(object_points, imagePoints1, imagePoints2, pointCounts, cv.fromarray(K1), cv.fromarray(distcoeffs1), cv.fromarray(K2), cv.fromarray(distcoeffs2), imageSize, cv.fromarray(R), cv.fromarray(T), cv.fromarray(E), cv.fromarray(F), flags = cv.CV_CALIB_FIX_INTRINSIC)
+#FileStorage fs1("mystereocalib.yml", FileStorage::WRITE);
+# fs1 << "CM1" << CM1;
+#fs1 << "CM2" << CM2;
+# #fs1 << "D1" << D1;
+#fs1 << "D2" << D2;
+#fs1 << "R" << R;
+#fs1 << "T" << T;
+#fs1 << "E" << E;
+#fs1 << "F" << F;
+print "Done Calibration\n"
+print cameraMatrix1,"\n"
+print R,"\n"
+print T,"\n"
+print distCoeffs1,"\n"
+print "Starting Rectification\n"
+R1 = np.zeros(shape=(3,3))
+R2 = np.zeros(shape=(3,3))
+P1 = np.zeros(shape=(3,3))
+P2 = np.zeros(shape=(3,3))
+
+#(roi1, roi2) = cv2.cv.StereoRectify(cameraMatrix1, distCoeffs1, cameraMatrix2, distCoeffs2,(width, height), R, T, R1, R2, P1, P2, Q=None, flags=cv2.cv.CV_CALIB_ZERO_DISPARITY, alpha=-1, newImageSize=(0, 0))
+cv2.stereoRectify(cameraMatrix1, distCoeffs1, cameraMatrix2, distCoeffs2,(width, height), R, T, R1, R2, P1, P2, Q=None, flags=cv2.CALIB_ZERO_DISPARITY, alpha=-1, newImageSize=(0,0))
+#stereoRectify(cameraMatrix1, distCoeffs1, cameraMatrix2, distCoeffs2,(width, height), R, T)
+
+#fs1 << "R1" << R1;
+#fs1 << "R2" << R2;
+#fs1 << "P1" << P1;
+#fs1 << "P2" << P2;
+#fs1 << "Q" << Q;
+
+print "Done Rectification\n"
+print "Applying Undistort\n"
+
+
+
+map1x, map1y = cv2.initUndistortRectifyMap(cameraMatrix1, distCoeffs1, R1, P1, (width, height), cv2.CV_32FC1)
+map2x, map2y = cv2.initUndistortRectifyMap(cameraMatrix2, distCoeffs2, R2, P2, (width, height), cv2.CV_32FC1)
+
+print "Undistort complete\n"
+
+cameraL = cv2.VideoCapture('C:/Users/peter/Desktop/ht_validation/calibration/intrinsic cal-left cam.avi')
+if cameraL.isOpened() == False:
+        print "Cannot open input video!"
+        exit()
+cameraL.set(1, 100) #CV_CAP_PROP_POS_FRAMES
+
+cameraR = cv2.VideoCapture('C:/Users/peter/Desktop/ht_validation/calibration/intrinsic cal-right cam.avi')
+if cameraR.isOpened() == False:
+        print "Cannot open input video!"
+        exit()
+cameraR.set(1, 100) #CV_CAP_PROP_POS_FRAMES
+
+while(True):
+    # grab the left frame
+    (grabbed, img1) = cameraL.read()
+    # grab the right frame
+    (grabbed, img2) = cameraR.read()
+
+    imgU1 = np.zeros((height,width,3), np.uint8)
+    imgU1 = cv2.remap(img1, map1x, map1y, cv2.INTER_LINEAR, imgU1, cv2.BORDER_CONSTANT, 0)
+    imgU2 = cv2.remap(img2, map2x, map2y, cv2.INTER_LINEAR)
+    cv2.imshow("imageL", img1);
+    cv2.imshow("imageR", img2);
+    cv2.imshow("image1L", imgU1);
+    cv2.imshow("image2R", imgU2);
+    k = cv2.waitKey(1000);
+    if(k==27):
+        break;
+
+cv2.destroyAllWindows()
+cameraL.release()
+cameraR.release()
