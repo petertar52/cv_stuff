7acab3c4e6587c046d64cd7afa3c11813417923f
Peter Tarca <ptarca@astrocsys.com>
2017-08-18 14:48:13 -0400

    use left camera calibration matrix and distcoeffs from stereo_cal

--- rotandtrans.py
+++ rotandtrans.py
@@ -34,22 +34,22 @@ object_points = np.array([
 focal_length = size[1]
 center = (size[1]/2, size[0]/2)
 camera_matrix = np.array(
-                         [[602.3300926, 0, 314.00816007],
-                         [0, 601.89164578, 265.7839791],
-                         [0, 0, 1]], dtype = "double"
+                         [[ 630.95421733,    0.0,          339.94466866],
+                          [   0.0,          630.95421733,  251.99814319],
+                          [   0.0,           0.0,          1.0        ]], dtype = "double"
                          )
  
 print "Camera Matrix :\n {0}".format(camera_matrix)
  
 #dist_coeffs = np.zeros((4,1)) # Assuming no lens distortion
-dist_coeffs = np.array([-0.13277752, 0.24566243, 0.00257889, -0.00288082, -0.35018818]) # lens distortion
+dist_coeffs = np.array([-0.11965687,  0.05348012,  0.0,          0.0,          0.0,          0.0,          0.0,  -0.36480494]) # lens distortion
 
 (success, rotation_vector, translation_vector) = cv2.solvePnP(object_points, image_points, camera_matrix, dist_coeffs, flags=cv2.CV_ITERATIVE)
  
 print "Rotation Vector:\n {0}".format(rotation_vector)
 print "Translation Vector:\n {0}".format(translation_vector)
  
-(nose_end_point2D, jacobian) = cv2.projectPoints(np.array([(1385.57, 1041.4, 2884.57)]), rotation_vector, translation_vector, camera_matrix, dist_coeffs)
+(nose_end_point2D, jacobian) = cv2.projectPoints(np.array([(1422.4, 1041.4, 2884.57)]), rotation_vector, translation_vector, camera_matrix, dist_coeffs)
  
 for p in image_points:
     cv2.circle(im, (int(p[0]), int(p[1])), 3, (0,0,255), -1)
